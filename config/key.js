19cb8aba1f799ecdd88947193b873e906c0c8ee5
PoojaLohar604 <130908223+PoojaLohar604@users.noreply.github.com>
2023-07-19 05:37:18 -0700

    Update key.js

--- config/key.js
+++ config/key.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI: "mongodb+srv://poojapanwar:mikkilohar1892001@cluster0.ojk4wgk.mongodb.net/"
+    MongoURI: "mongodb+srv://souhardyagayen99:I5MNSsLAifJJVhWP@cluster0.tjwzsru.mongodb.net/"
 }
